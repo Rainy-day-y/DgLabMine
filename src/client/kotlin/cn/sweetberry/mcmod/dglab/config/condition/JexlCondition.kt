ef77fff0c1d2ea756d3132b593f7b3a53057c63a
Rainy-day-y <rainy.daily@outlook.com>
2026-02-10 16:12:52 +0700

    fix: use try-catch to cast result to boolen safely

--- src/client/kotlin/cn/sweetberry/mcmod/dglab/config/condition/JexlCondition.kt
+++ src/client/kotlin/cn/sweetberry/mcmod/dglab/config/condition/JexlCondition.kt
@@ -4,6 +4,7 @@ import cn.sweetberry.mcmod.dglab.jexl.JexlEngine
 import cn.sweetberry.mcmod.dglab.jexl.toJexlContext
 import cn.sweetberry.mcmod.vitalsignals.network.damage.DamageData
 import me.shedaniel.autoconfig.annotation.ConfigEntry
+import org.slf4j.LoggerFactory
 
 data class JexlCondition(
     var expression: String = ""
@@ -13,6 +14,11 @@ data class JexlCondition(
 
     override fun matches(data: DamageData): Boolean {
         val context = data.toJexlContext()
-        return JexlEngine.getExpression(expression).evaluate(context) as Boolean
+        return try{
+            JexlEngine.getExpression(expression).evaluate(context) as Boolean
+        }catch (e: Exception){
+            LoggerFactory.getLogger(this::class.java).warn("A Bad Condition: ${ e.message }")
+            false
+        }
     }
 }
